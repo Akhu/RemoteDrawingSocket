07d26c829923b74b2824773f2ad213d7d2987d21
Anthony Da Cruz <anthony.dacruz@aboutgoods.net>
2017-01-24 15:09:46 +0100

    Peoples have ids and colors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var colors = ["#E53935","#8E24AA","#D81B60","#00897B","#FDD835","#039BE5"];
+
+var peoples = {};
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
@@ -13,12 +15,36 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
     console.log('user connected');
-    socket.on('connect', function(){
-       socket.emit("color", colors[Math.floor(Math.random()*100)]);
+
+     var id = socket.id;
+     var color = colors[Math.floor(Math.random()*5)];
+
+     peoples[id] = color;
+
+     socket.emit("me", {
+       "id" : id,
+       "color" : color
+     });
+
+     socket.broadcast.emit("newGuy", {
+       "id" : id,
+       "color" : color
+     });
+
+     console.log(peoples);
+
+    socket.on('clear', function(){
+      io.emit("clear");
     })
 
     socket.on('drawing', function(coordinates){
-      console.log(coordinates);
-      socket.broadcast.emit("receiveDrawing", coordinates);
+
+      var objectToSend = {
+       "coordinates" : coordinates,
+       "drawer" : socket.id
+     };
+     console.log(objectToSend);
+      socket.broadcast.emit("receiveDrawing", objectToSend);
+
     });
 });
