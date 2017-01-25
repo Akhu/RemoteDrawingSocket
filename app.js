33f8310d0e27020491fcabf1770987677898a1a6
Anthony <anthony.dacruz@aboutgoods.net>
2017-01-25 09:12:03 +0100

    LEs changements du serveurs

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var colors = ["#E53935","#8E24AA","#D81B60","#00897B","#FDD835","#039BE5"];
+var colors = ["#E53935","#8E24AA","#D81B60","#00897B","#FDD835","#039BE5","#E91E63","#2196F3","#3F51B5","#4CAF50","#FFC107","#FF9800","#FFEB3B"];
 
-var peoples = {};
+var peoples = new Array();
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
@@ -16,27 +16,28 @@ http.listen(3000, function(){
 io.on('connection', function(socket){
     console.log('user connected');
 
-     var id = socket.id;
-     var color = colors[Math.floor(Math.random()*5)];
+    //Show me your color
+    socket.on("login", function(){
+      var id = socket.id;
+      var color = colors[Math.floor(Math.random()*5)];
 
-     peoples[id] = color;
+      peoples.push({ "id" : id, "color" : color });
 
-     socket.emit("me", {
-       "id" : id,
-       "color" : color
-     });
+      socket.emit("me", {
+        "id" : id,
+        "color" : color
+      });
 
-     socket.broadcast.emit("newGuy", {
-       "id" : id,
-       "color" : color
-     });
+      io.emit("userList", peoples);
 
-     console.log(peoples);
+      console.log(peoples);
+    })
 
     socket.on('clear', function(){
       io.emit("clear");
     })
 
+    //Transfer coordinates
     socket.on('drawing', function(coordinates){
 
       var objectToSend = {
@@ -45,6 +46,19 @@ io.on('connection', function(socket){
      };
      console.log(objectToSend);
       socket.broadcast.emit("receiveDrawing", objectToSend);
-
     });
+
+    socket.on("disconnect", function(){
+        console.log("bye bye "+socket.id);
+        peoples.splice(arrayObjectIndexOf(peoples, socket.id, "id"), 1)
+    })
 });
+
+function arrayObjectIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+    	//console.log(myArray[i]);
+    	//console.log(myArray[i]);
+        if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+}
